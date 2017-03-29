55f9749e4325cdff430d1a00c261495ea34a8b9b
Winston Lindsay <wnlindsay@yahoo.com>
2017-03-29 20:25:14 +0000

    first part completed

--- image_getter.py
+++ image_getter.py
@@ -2,37 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
-def images():
+
+
+def getImg():
     url = "https://www.walmart.com/ip/54649026"
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     
-  
-    image = []
-
+    images = []
+    
     # This will look for a meta tag with the og:image property
     og_image = (soup.find('meta', property='og:image') or
                         soup.find('meta', attrs={'name': 'og:image'}))
     if og_image and og_image['content']:
-        print og_image['content']
         pass
-        #image.append(og_image['content'])
-        
+        #print og_image['content']
+        #print ''
     
     # This will look for a link tag with a rel attribute set to 'image_src'
     thumbnail_spec = soup.find('link', rel='image_src')
     if thumbnail_spec and thumbnail_spec['href']:
-        print thumbnail_spec['href']
-        #print ''
         pass
-        #image.append(thumbnail_spec['href'])
-        
-        
-        for ima in soup.findAll("img", src=True):
-            image.appends(image["src"])
-        
+        #print thumbnail_spec['href']
+        #print ''
     
-        return image
+    #image = """<img src="%s"><br />"""
+    for img in soup.findAll("img", src=True):
+        # eliminate duplicate links
+        if img["src"] not in images:
+            images.append(img["src"])
        
-print images()
-        
+    return images
+
+print  getImg()
